712f4d63489abeaaf7f61a4b70acdac0219c28be
chise0713 <serika@milkdog.onmicrosoft.com>
2023-12-03 07:17:18 +0800

    Feat: Add `reserved` field support.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # warp-reg.sh
-Thanks: [@badafans](https://github.com/badafans/warp-reg)<br>
+Thanks: [XTLS/Xray-install](https://github.com/XTLS/Xray-install),[@fscarmen](https://github.com/fscarmen/), [fscarmen/warp](https://gitlab.com/fscarmen/warp/), [@badafans](https://github.com/badafans), [badafans/warp-reg](https://github.com/badafans/warp-reg)<br>
 ## Run
 ```bash
 bash -c "$(curl -L warp-reg.vercel.app)"
@@ -8,12 +8,14 @@ bash -c "$(curl -L warp-reg.vercel.app)"
 ```json
 {
     "endpoint":{
-       "v4": "162.159.192.3",
-       "v6": "[2606:4700:d0::a29f:c003]",
+       "v4": "162.159.192.10",
+       "v6": "[2606:4700:d0::a29f:c00a]",
     },
-    "private_key": "+K7SzFKYxwKJrRXgcWG7/fT4X3fmPRnhGHbKkC2HLWg=",
+    "reserved_dec": [225,76,129],
+    "reserved_hex": "0xe14c81",
+    "private_key": "IDJ/MHXvu5W29fNxy13uyqluSM8TgoYtwuWmWBVGR24=",
     "public_key": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
     "v4": "172.16.0.2",
-    "v6": "2606:4700:110:8c53:a2ad:e293:1bcb:401d"
+    "v6": "2606:4700:110:8aa7:cda2:ffbd:c68e:b126"
 }
 ```
--- warp-reg.sh
+++ warp-reg.sh
@@ -59,23 +59,39 @@ reg() {
         | python3 -m json.tool | sed "/\"account_type\"/i\         \"private_key\": \"$private_key\","
 }
 
+reservd() {
+    client_id=$(echo "$warp_info" | grep 'client_id' | cut -d\" -f4)
+    reserved_hex=$(echo "$client_id" | base64 -d | xxd -p)
+    reserved_dec=$(echo "$reserved_hex" | fold -w2 | while read HEX; do printf '%d ' "0x${HEX}"; done | awk '{print "["$1", "$2", "$3"]"}')
+    echo -e "{\n    \"reserved_dec\": $reserved_dec,"
+    echo -e "    \"reserved_hex\": \"0x$reserved_hex\","
+    echo -e "    \"client_id\": \"$client_id\"\n}"
+}
+format (){
+    echo "{
+    \"endpoint\":{"
+    echo "$warp_info" | grep -P "(v4|v6)" | grep -vP "(\"v4\": \"172.16.0.2\"|\"v6\": \"2)" | sed "s/ //g" | sed 's/:"/: "/g' | sed 's/^"/       "/g' | sed 's/:0",$/",/g'
+    echo '    },'
+    echo "$warp_reservd" | grep -P "reserved" | sed "s/ //g" | sed 's/:"/: "/g' | sed 's/:\[/: \[/g' | sed 's/^"/    "/g'
+    echo "$warp_info" | grep -P "(private_key|public_key|\"v4\": \"172.16.0.2\"|\"v6\": \"2)" | sed "s/ //g" | sed 's/:"/: "/g' | sed 's/^"/    "/g'
+    echo "}"
+}
 main() {
     package_manager
     install_software 'wireguard-tools' 'wg'
-    warp_info=$(reg)
-    exit=$?
-    if [[ $exit != 0 ]];then
+    warp_info=$(reg) && exit_code=$?
+    if [[ $exit_code != 0 ]];then
         echo "$warp_info"
         echo -e "${ERROR}ERROR:${END} \"reg\" function returned a none zero exit code, exiting."
-        exit $exit
-    else
-        echo "{
-    \"endpoint\":{"
-        echo "$warp_info" | grep -P "(v4|v6)" | grep -vP "(\"v4\": \"172.16.0.2\"|\"v6\": \"2)" | sed "s/ //g" | sed 's/:"/: "/g' | sed 's/^"/       "/g' | sed 's/:0",$/",/g'
-        echo '    },'
-        echo "$warp_info" | grep -P "(private_key|public_key|\"v4\": \"172.16.0.2\"|\"v6\": \"2)" | sed "s/ //g" | sed 's/:"/: "/g' | sed 's/^"/    "/g'
-        echo "}"
+        exit $exit_code
     fi
+    warp_reservd=$(reservd) && exit_code=$?
+    if [[ $exit_code != 0 ]];then
+        echo "$warp_reservd"
+        echo -e "${ERROR}ERROR:${END} \"reservd\" function returned a none zero exit code, exiting."
+        exit $exit_code
+    fi
+    format
 }
 
 main
