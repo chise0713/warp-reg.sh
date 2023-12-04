854fbcde7adae09139d77e6b3060d087afbfc036
chise0713 <serika@milkdog.onmicrosoft.com>
2023-12-05 03:58:31 +0800

    Chore(feat): Using wgg-go as wireguard keygenerator

--- warp-reg.sh
+++ warp-reg.sh
@@ -3,52 +3,10 @@ ERROR="\e[1;31m"
 WARN="\e[93m"
 END="\e[0m"
 
-package_manager() {
-    if [[ "$(type -P apt)" ]]; then
-        PACKAGE_MANAGEMENT_INSTALL='apt -y --no-install-recommends install'
-        PACKAGE_MANAGEMENT_REMOVE='apt purge'
-        package_provide_tput='ncurses-bin'
-    elif [[ "$(type -P dnf)" ]]; then
-        PACKAGE_MANAGEMENT_INSTALL='dnf -y install'
-        PACKAGE_MANAGEMENT_REMOVE='dnf remove'
-        package_provide_tput='ncurses'
-    elif [[ "$(type -P yum)" ]]; then
-        PACKAGE_MANAGEMENT_INSTALL='yum -y install'
-        PACKAGE_MANAGEMENT_REMOVE='yum remove'
-        package_provide_tput='ncurses'
-    elif [[ "$(type -P zypper)" ]]; then
-        PACKAGE_MANAGEMENT_INSTALL='zypper install -y --no-recommends'
-        PACKAGE_MANAGEMENT_REMOVE='zypper remove'
-        package_provide_tput='ncurses-utils'
-    elif [[ "$(type -P pacman)" ]]; then
-        PACKAGE_MANAGEMENT_INSTALL='pacman -Syu --noconfirm'
-        PACKAGE_MANAGEMENT_REMOVE='pacman -Rsn'
-        package_provide_tput='ncurses'
-     elif [[ "$(type -P emerge)" ]]; then
-        PACKAGE_MANAGEMENT_INSTALL='emerge -qv'
-        PACKAGE_MANAGEMENT_REMOVE='emerge -Cv'
-        package_provide_tput='ncurses'
-    else
-        echo -e "${ERROR}ERROR:${END} The script does not support the package manager in this operating system."
-        exit 1
-    fi
-}
-
-install_software() {
-    package_name="$1"
-    file_to_detect="$2"
-    type -P "$file_to_detect" > /dev/null 2>&1 && return || echo -e "${WARN}WARN:${END} $package_name not installed, installing."
-    if ${PACKAGE_MANAGEMENT_INSTALL} "$package_name" >/dev/null 2>&1; then
-        echo "INFO: $package_name is installed."
-    else
-        echo -e "${ERROR}ERROR:${END} Installation of $package_name failed, please check your network."
-        exit 1
-    fi
-}
-
 reg() {
-    private_key=$(wg genkey)
-    public_key=$(echo "$private_key" | wg pubkey)
+	output=$(/tmp/wgg-go)
+    private_key=$(echo "$output"|grep 'priv:'| sed 's/priv: //g')
+    public_key=$(echo "$output"|grep 'pub:'| sed 's/pub: //g')
     curl -X POST 'https://api.cloudflareclient.com/v0a2158/reg' -sL --tlsv1.3 \
     -H 'CF-Client-Version: a-7.21-0721' -H 'Content-Type: application/json' \
     -d \
@@ -68,7 +26,7 @@ reservd() {
     echo -e "    \"reserved_str\": \"$reserved_str\"\n}"
 }
 
-format (){
+format() {
     echo "{
     \"endpoint\":{"
     echo "$warp_info" | grep -P "(v4|v6)" | grep -vP "(\"v4\": \"172.16.0.2\"|\"v6\": \"2)" | sed "s/ //g" | sed 's/:"/: "/g' | sed 's/^"/       "/g' | sed 's/:0",$/",/g'
@@ -78,23 +36,35 @@ format (){
     echo "}"
 }
 
+get-wgg-go() {
+	curl -o /tmp/wgg-go -sL https://github.com/chise0713/warp-reg.sh/blob/main/wgg-go/wgg-go?raw=true
+	curl -o /tmp/wgg-go.sha256sum -sL https://github.com/chise0713/warp-reg.sh/blob/main/wgg-go/wgg-go.sha256sum?raw=true
+	local EXPECT_HASH=$(cat "/tmp/wgg-go.sha256sum" | awk '{print $1}')
+	local ACTUAL_HASH=$(sha256sum "/tmp/wgg-go" | awk '{print $1}')
+	if ! [[ $ACTUAL_HASH == $EXPECT_HASH ]];then
+	    echo -e "${ERROR}ERROR:${END} sha256sum Failed.\nRemoving tmpfiles\nExiting."
+	    rm /tmp/wgg-go /tmp/wgg-go.sha256sum -f
+	    exit 1
+	fi
+	chmod +x /tmp/wgg-go
+}
+
 main() {
-    package_manager
-    install_software 'wireguard-tools' 'wg'
+	get-wgg-go
+	sleep 5 && rm /tmp/wgg-go /tmp/wgg-go.sha256sum -f &
     warp_info=$(reg) && exit_code=$?
     if [[ $exit_code != 0 ]];then
         echo "$warp_info"
-        echo -e "${ERROR}ERROR:${END} \"reg\" function returned a none zero exit code, exiting."
+        echo -e "${ERROR}ERROR:${END} \"reg\" function returned with $exit_code, exiting."
         exit $exit_code
     fi
     warp_reservd=$(reservd) && exit_code=$?
     if [[ $exit_code != 0 ]];then
         echo "$warp_reservd"
-        echo -e "${ERROR}ERROR:${END} \"reservd\" function returned a none zero exit code, exiting."
+        echo -e "${ERROR}ERROR:${END} \"reservd\" function returned with $exit_code, exiting."
         exit $exit_code
     fi
     format
 }
 
 main
-
