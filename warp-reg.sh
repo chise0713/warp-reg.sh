101203301a78cef8e81273af5c705647d6841f9e
chise0713 <serika@milkdog.onmicrosoft.com>
2023-12-06 12:00:40 +0800

    Chore(fix): Auto install denpendencies

--- warp-reg.sh
+++ warp-reg.sh
@@ -3,7 +3,51 @@ ERROR="\e[1;31m"
 WARN="\e[93m"
 END="\e[0m"
 
+package_manager() {
+    if [[ "$(type -P apt)" ]]; then
+        PACKAGE_MANAGEMENT_INSTALL='apt -y --no-install-recommends install'
+        PACKAGE_MANAGEMENT_REMOVE='apt purge'
+        package_provide_tput='ncurses-bin'
+    elif [[ "$(type -P dnf)" ]]; then
+        PACKAGE_MANAGEMENT_INSTALL='dnf -y install'
+        PACKAGE_MANAGEMENT_REMOVE='dnf remove'
+        package_provide_tput='ncurses'
+    elif [[ "$(type -P yum)" ]]; then
+        PACKAGE_MANAGEMENT_INSTALL='yum -y install'
+        PACKAGE_MANAGEMENT_REMOVE='yum remove'
+        package_provide_tput='ncurses'
+    elif [[ "$(type -P zypper)" ]]; then
+        PACKAGE_MANAGEMENT_INSTALL='zypper install -y --no-recommends'
+        PACKAGE_MANAGEMENT_REMOVE='zypper remove'
+        package_provide_tput='ncurses-utils'
+    elif [[ "$(type -P pacman)" ]]; then
+        PACKAGE_MANAGEMENT_INSTALL='pacman -Syu --noconfirm'
+        PACKAGE_MANAGEMENT_REMOVE='pacman -Rsn'
+        package_provide_tput='ncurses'
+     elif [[ "$(type -P emerge)" ]]; then
+        PACKAGE_MANAGEMENT_INSTALL='emerge -qv'
+        PACKAGE_MANAGEMENT_REMOVE='emerge -Cv'
+        package_provide_tput='ncurses'
+    else
+        echo -e "${ERROR}ERROR:${END} The script does not support the package manager in this operating system."
+        exit 1
+    fi
+}
+
+install_software() {
+    package_name="$1"
+    file_to_detect="$2"
+    type -P "$file_to_detect" > /dev/null 2>&1 && return || echo -e "${WARN}WARN:${END} $package_name not installed, installing." && sleep 1
+    if ${PACKAGE_MANAGEMENT_INSTALL} "$package_name"; then
+        echo "INFO: $package_name is installed."
+    else
+        echo -e "${ERROR}ERROR:${END} Installation of $package_name failed, please check your network."
+        exit 1
+    fi
+}
+
 reg() {
+    set -e
 	output=$(/tmp/wgg-go)
     private_key=$(echo "$output"|grep 'priv:'| sed 's/priv: //g')
     public_key=$(echo "$output"|grep 'pub:'| sed 's/pub: //g')
@@ -17,7 +61,8 @@ reg() {
         | python3 -m json.tool | sed "/\"account_type\"/i\         \"private_key\": \"$private_key\","
 }
 
-reservd() {
+reserved() {
+    set -e
     reserved_str=$(echo "$warp_info" | grep 'client_id' | cut -d\" -f4)
     reserved_hex=$(echo "$reserved_str" | base64 -d | xxd -p)
     reserved_dec=$(echo "$reserved_hex" | fold -w2 | while read HEX; do printf '%d ' "0x${HEX}"; done | awk '{print "["$1", "$2", "$3"]"}')
@@ -31,7 +76,7 @@ format() {
     \"endpoint\":{"
     echo "$warp_info" | grep -P "(v4|v6)" | grep -vP "(\"v4\": \"172.16.0.2\"|\"v6\": \"2)" | sed "s/ //g" | sed 's/:"/: "/g' | sed 's/^"/       "/g' | sed 's/:0",$/",/g'
     echo '    },'
-    echo "$warp_reservd" | grep -P "reserved" | sed "s/ //g" | sed 's/:"/: "/g' | sed 's/:\[/: \[/g' | sed 's/\([0-9]\+\),\([0-9]\+\),\([0-9]\+\)/\1, \2, \3/' | sed 's/^"/    "/g' | sed 's/"$/",/g'
+    echo "$warp_reserved" | grep -P "reserved" | sed "s/ //g" | sed 's/:"/: "/g' | sed 's/:\[/: \[/g' | sed 's/\([0-9]\+\),\([0-9]\+\),\([0-9]\+\)/\1, \2, \3/' | sed 's/^"/    "/g' | sed 's/"$/",/g'
     echo "$warp_info" | grep -P "(private_key|public_key|\"v4\": \"172.16.0.2\"|\"v6\": \"2)" | sed "s/ //g" | sed 's/:"/: "/g' | sed 's/^"/    "/g'
     echo "}"
 }
@@ -52,18 +97,21 @@ get-wgg-go() {
 }
 
 main() {
+    package_manager
+    install_software "xxd" "xxd"
+    install_software "python3" "python3"
 	get-wgg-go
 	sleep 5 && rm /tmp/wgg-go /tmp/wgg-go.sha256sum -f &
-    warp_info=$(reg) && exit_code=$?
+    warp_info=$(reg) ; exit_code=$?
     if [[ $exit_code != 0 ]];then
         echo "$warp_info"
         echo -e "${ERROR}ERROR:${END} \"reg\" function returned with $exit_code, exiting."
         exit $exit_code
     fi
-    warp_reservd=$(reservd) && exit_code=$?
+    warp_reserved=$(reserved) ; exit_code=$?
     if [[ $exit_code != 0 ]];then
-        echo "$warp_reservd"
-        echo -e "${ERROR}ERROR:${END} \"reservd\" function returned with $exit_code, exiting."
+        echo "$warp_reserved"
+        echo -e "${ERROR}ERROR:${END} \"reserved\" function returned with $exit_code, exiting."
         exit $exit_code
     fi
     format
